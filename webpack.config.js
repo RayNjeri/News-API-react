53f301eb10bdebc65d98ac8eef8c4adc557caea9
Rachael Njeri <rachael.njeri@andela.com>
2017-04-27 19:22:58 +0300

    initial commit
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+}
