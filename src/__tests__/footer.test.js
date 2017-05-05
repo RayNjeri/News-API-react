7548cb8f1f0ec3a9a6355bae339011220f570b6b
Rachael Njeri <rachael.njeri@andela.com>
2017-05-05 10:45:37 +0300

    add footer unit test
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

---
+++ src/__tests__/footer.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import shallowToJSON from 'enzyme-to-json';
+import Footer from '../components/Footer';
+
+describe('Home Component', () => {
+    it('Should render a footer items', () => {
+        const footer = shallow(<Footer />);
+        const tree = shallowToJSON(footer);
+        expect(tree).toMatchSnapshot();
+    })
+})
