74694645567d1dc73b2f6791d1621ddf844b9888
Rachael Njeri <rachael.njeri@andela.com>
2017-05-11 16:19:08 +0300

    add the footer component
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <ul>
+                        <li><Link to="/About Us">About Us</Link></li>
+                        <li><Link to='/Terms & Conditions'>Terms & Conditions</Link></li>
+                        <li><Link to="/Help">Help</Link></li>
+                        <li><Link to="/Contact Us">Contact Us</Link></li>
+                    </ul>
+                </Router>
+            </div>
+        );
+    }
+}
+
