288d989733882b16eee077d8f56706cd39cf8d69
Rachael Njeri <rachael.njeri@andela.com>
2017-05-17 17:25:00 +0300

    configure scss and styling loaders
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
     module: {
         rules: [
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.(css)$|scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
         ]
     },
     plugins: [HtmlWebpackPluginConfig]
