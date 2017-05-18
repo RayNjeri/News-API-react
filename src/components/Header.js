7c02c2c2b3bcd3bf948bcaf814e36e741bccc043
Rachael Njeri <rachael.njeri@andela.com>
2017-05-18 13:13:42 +0300

    implement login's sign in button
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 
 export default class Header extends React.Component {
   render() {
@@ -8,4 +8,4 @@ export default class Header extends React.Component {
       </div>
     );
   }
-}
+}*/
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import Header from './Header';
+// import Header from './Header';
 import Content from './Content';
-import Login from './Login';
 import Footer from './Footer';
 
 export default class Home extends React.Component {
-  render() {
-    return (
-      <div className="home">
-        <div className="headers"><Header /></div>
-        <div className="login"><Login /></div>
-        <div className="content"><Content /></div>
-        <Footer />
-      </div >
-    );
-  }
+    render() {
+        return (
+            <div className="home">
+                {/*<div className="headers"><Header /></div>*/}
+                <div className="content"><Content /></div>
+                <Footer />
+            </div >
+        );
+    }
 }
