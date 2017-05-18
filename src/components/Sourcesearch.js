7a9e9ac0738358df277e8638832bcab1930f96b0
Rachael Njeri <rachael.njeri@andela.com>
2017-05-18 09:50:00 +0300

    remove re-rendering of sources list
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- src/components/Sourcesearch.js
+++ src/components/Sourcesearch.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Sources from './Sources';
 import SearchInput, { createFilter } from 'react-search-input';
+import Axios from 'axios';
 
 const KEY_TO_FILTERS = ['sources.name'];
 
@@ -13,19 +14,25 @@ export default class Search extends Component {
             searchTerm: ''
         };
     }
+    componentDidMount() {
+        Axios.get('https://newsapi.org/v1/sources')
+            .then((result) => {
+                console.log('getting result: ', result);
+                this.setState({
+                    sources: result.data.sources
+                });
+            });
+    }
+
     searchUpdated(term) {
         this.setState({ searchTerm: term });
     }
+
     render() {
-        const filteredSources = sources.filter(createFilter(this.state.searchTerm, KEY_TO_FILTERS));
+        const filteredSources = this.state.sources.filter(createFilter(this.state.searchTerm, KEY_TO_FILTERS));
         return (
             <div>
                 <SearchInput className="search-input" onChange={this.searchUpdated} />
-                {filteredSources.map(sources => {
-                    return (
-                        <div className="from">{sources.name}</div>
-                    );
-                })}
             </div>
         );
     }
