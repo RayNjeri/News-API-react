17424cd85b0d38e3cdc4e6274a93b7440f6a3e1e
Rachael Njeri <rachael.njeri@andela.com>
2017-05-18 09:53:38 +0300

    render source search
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Sources from './Sources';
 import Articles from './Articles';
+import Search from './Sourcesearch';
 
 
 export default class Content extends React.Component {
@@ -10,6 +11,7 @@ export default class Content extends React.Component {
             <div className="content">
                 <div className="row">
                     <div className="col-sm-4" ><Sources /></div>
+                    <div className="Search"><Search /></div>
                     <div className="col-sm-8"><Articles /></div>
                 </div>
             </div >
